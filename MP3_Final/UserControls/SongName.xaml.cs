f55704d783fbdba55aa4f0c558362cd99c7ab2b8
tamxtanh <21521411@gm.uit.edu.vn>
2022-11-24 22:29:30 +0700

    Add new code

---
+++ MP3_Final/UserControls/SongName.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MP3_Final.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SongItem.xaml
+    /// </summary>
+    public partial class SongName : UserControl
+    {
+        public SongName()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register
+            ("Title", typeof(string), typeof(SongName));
+
+        public string Singer
+        {
+            get { return (string)GetValue(SingerProperty); }
+            set { SetValue(SingerProperty, value); }
+        }
+
+        public static readonly DependencyProperty SingerProperty = DependencyProperty.Register
+            ("Singer", typeof(string), typeof(SongName));
+
+
+
+        public string Number
+        {
+            get { return (string)GetValue(NumberProperty); }
+            set { SetValue(NumberProperty, value); }
+        }
+
+        public static readonly DependencyProperty NumberProperty = DependencyProperty.Register
+            ("Number", typeof(string), typeof(SongName));
+
+
+
+        public string Time
+        {
+            get { return (string)GetValue(TimeProperty); }
+            set { SetValue(TimeProperty, value); }
+        }
+
+        public static readonly DependencyProperty TimeProperty = DependencyProperty.Register
+            ("Time", typeof(string), typeof(SongName));
+
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register
+            ("IsActive", typeof(bool), typeof(SongName));
+    }
+}
