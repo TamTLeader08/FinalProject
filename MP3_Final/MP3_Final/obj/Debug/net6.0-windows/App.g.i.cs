3d99de337dbe7cb5b1d72530d6f84f00ee371fb5
tamxtanh <21521411@gm.uit.edu.vn>
2022-11-24 21:48:51 +0700

    Complete UI

--- MP3_Final/MP3_Final/obj/Debug/net6.0-windows/App.g.i.cs
+++ MP3_Final/MP3_Final/obj/Debug/net6.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace MP3_Final {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace MP3_Final {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/MP3_Final;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MP3_Final;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace MP3_Final {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public static void Main() {
             MP3_Final.App app = new MP3_Final.App();
             app.InitializeComponent();
